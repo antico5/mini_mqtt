54890ab74ae87ed04eeea0375c05d282dba785ce
Armando Andini <armando.andini@hotmail.com>
2016-07-30 20:19:35 -0300

    Test for ConnackPacket with refused connection. Add error messages depending on return code.

--- packet.rb
+++ packet.rb
@@ -40,6 +40,13 @@ end
 class ConnackPacket < Packet
   attr_reader :return_code
 
+  ERRORS = { 1 => "unacceptable protocol version",
+             2 => "identifier rejected",
+             3 => "server unavailable",
+             4 => "bad username or password",
+             5 => "not authorized" }
+
+
   def read_variable_header
     @session_present = @stream.readbyte & 0x01
     @return_code = @stream.readbyte
@@ -52,6 +59,11 @@ class ConnackPacket < Packet
   def accepted?
     @return_code == 0
   end
+
+  def error_message
+    ERRORS[@return_code]
+  end
+
 end
 
 class PublishPacket < Packet
--- test.rb
+++ test.rb
@@ -86,7 +86,15 @@ class ConnackPacketTest < MiniTest::Test
   def test_decode_accepted_connection
     packet = ConnackPacket.new
     packet.decode "\x01\x00".to_stream
-    assert true, packet.session_present?
-    assert true, packet.accepted?
+    assert packet.session_present?
+    assert packet.accepted?
+  end
+
+  def test_decode_refused_connection
+    packet = ConnackPacket.new
+    packet.decode "\x00\x04".to_stream
+    assert_equal false, packet.session_present?
+    assert_equal false, packet.accepted?
+    assert_match /bad username or password/, packet.error_message
   end
 end
