52bdefd5f8c1adb034558a18a5f9161e108800f7
Armando Andini <armando.andini@hotmail.com>
2016-07-30 10:35:26 -0300

    Create PacketReader and Packet as different classes.

--- packet.rb
+++ packet.rb
@@ -1,59 +1,48 @@
-PACKET_CLASSES = { 1 => :connect,
-                   2 => :connack,
-                   3 => :publish,
-                   4 => :puback,
-                   5 => :pubrec,
-                   6 => :pubrel,
-                   7 => :pubcomp,
-                   8 => :subscribe,
-                   9 => :suback,
-                   10 => :unsubscribe,
-                   11 => :unsuback,
-                   12 => :pingreq,
-                   13 => :pingresp,
-                   14 => :disconnect }
-
 class Packet
-  MAX_LENGTH_MULTIPLIER = 128 ** 3
-
-  attr_reader :type, :flags, :length, :body
-
-  def initialize stream
-    first_byte = stream.readbyte
-    @type = PACKET_CLASSES[ first_byte >> 4 ]
-    @flags = first_byte & 0xf
-    @length = decode_length stream
-    @body = stream.read @length
-  end
-
-  private
-
-  def encode_length length
-    encoded = ""
-    while length > 0
-      encoded_byte = length % 128
-      length = length / 128
-      encoded_byte |= 128 if length > 0
-      encoded << encoded_byte.chr
-    end
-    encoded
-  end
-
-  def decode_length stream
-    length = 0
-    multiplier = 1
-    while encoded_byte = stream.readbyte
-      length += (encoded_byte & 0x7f) * multiplier
-      break if encoded_byte & 0x80 == 0
-      multiplier *= 128
-      raise "Malformed remaining length" if multiplier > MAX_LENGTH_MULTIPLIER
-    end
-    length
+  def initialize body, flags = 0
+    @body = body
+    @flags = flags
   end
 end
 
-stream = StringIO.new "\x13\x09holaperro"
-p1 = Packet.new stream
+class ConnectPacket < Packet
+end
 
+class ConnackPacket < Packet
+end
 
+class PublishPacket < Packet
+end
 
+class PubackPacket < Packet
+end
+
+class PubrecPacket < Packet
+end
+
+class PubrelPacket < Packet
+end
+
+class PubcompPacket < Packet
+end
+
+class SubscribePacket < Packet
+end
+
+class SubackPacket < Packet
+end
+
+class UnsubscribePacket < Packet
+end
+
+class UnsubackPacket < Packet
+end
+
+class PingreqPacket < Packet
+end
+
+class PingrespPacket < Packet
+end
+
+class DisconnectPacket < Packet
+end
---
+++ packet_reader.rb
@@ -0,0 +1,59 @@
+require 'packet'
+
+PACKET_CLASSES = { 1 => ConnectPacket,
+                   2 => ConnackPacket,
+                   3 => PublishPacket,
+                   4 => PubackPacket,
+                   5 => PubrecPacket,
+                   6 => PubrelPacket,
+                   7 => PubcompPacket,
+                   8 => SubscribePacket,
+                   9 => SubackPacket,
+                   10 => UnsubscribePacket,
+                   11 => UnsubackPacket,
+                   12 => PingreqPacket,
+                   13 => PingrespPacket,
+                   14 => DisconnectPacket }
+
+class PacketReader
+  MAX_LENGTH_MULTIPLIER = 128 ** 3
+
+  def initialize stream
+    @stream = stream
+  end
+
+  def get_packet
+    first_byte = @stream.readbyte
+    @packet_class = PACKET_CLASSES[ first_byte >> 4 ]
+    @flags = first_byte & 0xf
+    @length = decode_length @stream
+    @body = @stream.read @length
+    @packet_class.new @body, @flags
+  end
+
+  private
+
+  def encode_length length
+    encoded = ""
+    while length > 0
+      encoded_byte = length % 128
+      length = length / 128
+      encoded_byte |= 128 if length > 0
+      encoded << encoded_byte.chr
+    end
+    encoded
+  end
+
+  def decode_length stream
+    length = 0
+    multiplier = 1
+    while encoded_byte = stream.readbyte
+      length += (encoded_byte & 0x7f) * multiplier
+      break if encoded_byte & 0x80 == 0
+      multiplier *= 128
+      raise "Malformed remaining length" if multiplier > MAX_LENGTH_MULTIPLIER
+    end
+    length
+  end
+end
+
