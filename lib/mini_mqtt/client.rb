2b90f8ad40b80266a05709fdf50356f9b5ce9e00
Armando Andini <armando.andini@hotmail.com>
2016-08-05 12:54:55 -0300

    Client class development.

---
+++ lib/mini_mqtt/client.rb
@@ -0,0 +1,53 @@
+require 'socket'
+
+module MiniMqtt
+  class Client
+    attr_accessor :host, :port, :user, :password
+
+    def initialize params = {}
+      @host = params[:host]
+      @port = params[:port]
+      @user = params[:user]
+      @password = params[:password]
+    end
+
+    def connect
+      # Create socket and packet handler
+      @socket = TCPSocket.new @host, @port
+      @packet_handler = PacketHandler.new @socket
+      @packet_handler.debug = true
+
+      #Spawn read thread to receive incoming packets
+      spawn_read_thread!
+
+      # Send ConnectPacket
+      @packet_handler.write_packet ConnectPacket.new user: @user,
+        password: @password
+
+      # Give control to caller
+      yield
+
+      # Send DisconnectPacket and kill read thread.
+      @packet_handler.write_packet DisconnectPacket.new
+      kill_read_thread!
+    end
+
+    private
+
+      def spawn_read_thread!
+        @read_thread = Thread.new do
+          begin
+            loop do
+              @packet_handler.get_packet
+            end
+          rescue Exception => e
+            puts e.inspect
+          end
+        end
+      end
+
+      def kill_read_thread!
+        @read_thread.kill
+      end
+  end
+end
