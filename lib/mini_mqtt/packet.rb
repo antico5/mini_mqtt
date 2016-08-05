29626609ccefb77ab95ac634d07b4fc1a8c0b315
Armando Andini <armando.andini@hotmail.com>
2016-08-05 10:38:27 -0300

    UnsubscribePacket implementation.

--- lib/mini_mqtt/packet.rb
+++ lib/mini_mqtt/packet.rb
@@ -191,6 +191,26 @@ module MiniMqtt
   end
 
   class UnsubscribePacket < Packet
+    attr_accessor :packet_id, :topics
+
+    def flags
+      0b0010
+    end
+
+    def initialize params = {}
+      @topics = params[:topics]
+    end
+
+    def build_variable_header
+      @packet_id ||= new_packet_id
+      ushort @packet_id
+    end
+
+    def build_payload
+      @topics.map do |topic|
+        mqtt_utf8_encode(topic)
+      end.join
+    end
   end
 
   class UnsubackPacket < Packet
