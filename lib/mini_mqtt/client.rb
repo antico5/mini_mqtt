67d4573d38648d461fa7bb8542dae08fdf4c6407
Armando Andini <armando.andini@hotmail.com>
2016-08-10 10:37:29 -0300

    Remove session class. Move all back to client.

--- lib/mini_mqtt/client.rb
+++ lib/mini_mqtt/client.rb
@@ -29,14 +29,73 @@ module MiniMqtt
       connack = @packet_handler.get_packet
 
       if connack.accepted?
-        Session.new @packet_handler
+        @received_messages = []
+        @last_ping_response = Time.now
+        spawn_read_thread!
+        spawn_keepalive_thread!
       else
         raise StandardError.new(connack.error)
       end
     end
 
+    def subscribe topic
+      packet = SubscribePacket.new topics: {topic => 0}
+      @packet_handler.write_packet packet
+    end
+
+    def publish topic, message
+      packet = PublishPacket.new topic: topic, message: message
+      @packet_handler.write_packet packet
+    end
+
+    def disconnect
+      # Send DisconnectPacket, close socket and kill threads
+      @packet_handler.write_packet DisconnectPacket.new
+      kill_threads!
+      @socket.close
+    end
+
+    private
+
+      def handle_received_packet packet
+        case packet
+        when PingrespPacket
+          @last_ping_response = Time.now
+        end
+      end
+
       def generate_client_id
         "id_#{ rand(10000).to_s }"
+      end
+
+      def spawn_read_thread!
+        @read_thread = Thread.new do
+          begin
+            loop do
+              # read packet from socket and handle it
+              handle_received_packet @packet_handler.get_packet
+            end
+          rescue Exception => e
+            puts e.inspect
+          end
+        end
+      end
+
+      def spawn_keepalive_thread!
+        @keepalive_thread = Thread.new do
+          loop do
+            @handler.write_packet PingreqPacket.new
+            sleep @keep_alive
+            if Time.now - @last_ping_response > @keep_alive
+              puts "SERVER NOT RESPONDING TO PING."
+            end
+          end
+        end
+      end
+
+      def kill_threads!
+        @read_thread.kill
+        @keepalive_thread.kill
       end
   end
 end
