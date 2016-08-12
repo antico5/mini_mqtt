ab60421987d81fe61c72a502906750ca06ccb9b8
Armando Andini <armando.andini@hotmail.com>
2016-08-11 21:01:16 -0300

    Integration tests for client.

--- test/client_test.rb
+++ test/client_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'socket'
 
 class ClientTest < MiniTest::Test
   def setup
@@ -6,10 +7,28 @@ class ClientTest < MiniTest::Test
       client_id: 'myclient', clean_session: false
   end
 
-  def test_connect
-    session = @client.connect
-    assert session
-    assert session.disconnect
+  def test_mosquitto_server_is_running
+    socket = TCPSocket.new 'localhost', 1883
+    assert !socket.closed?
+    socket.close
+  end
+
+  def test_connect_and_disconnect
+    @client.connect
+    assert @client.connected?
+    @client.disconnect
+    refute @client.connected?
+  end
+
+  def test_subscribe_and_publish
+    @client.connect
+    @client.subscribe '/test'
+    @client.publish '/test', 'hi'
+    @client.get_message do |msg, topic|
+      assert_equal '/test', topic
+      assert_equal 'hi', msg
+    end
+    @client.disconnect
   end
 end
 
